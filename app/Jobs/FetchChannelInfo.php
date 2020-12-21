063465373c201c1b14bbf4eb873f290ec0e8e4f9
Jon O <jon1213@gmail.com>
2020-12-20 23:10:52 -0800

    Adding some notes in comments for discussion

--- app/Jobs/FetchChannelInfo.php
+++ app/Jobs/FetchChannelInfo.php
@@ -32,5 +32,9 @@ class FetchChannelInfo implements ShouldQueue
     public function handle()
     {
         $channel->getSubscriberInfo();
+
+        // NB: we use AllChannelInfo to call this job for each channel in order to allow great flexibility in the future. For example, we might end up separating jobs into different queues for the sake of load balancing. Or we might need to send each of these jobs out separately in order to not overload Youtube's request quota
+
+        // It's also good design and good separation of concerns. This job just cares about fetching info for one channel. If something needs to change about that job, it can happen here. Any errors that occur for that one channel happen here, and don't bring the whole process to a halt; that just means this one job goes in the failed jobs queue
     }
 }
